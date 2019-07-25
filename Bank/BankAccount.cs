64aa8675c66a061f039a51bf096b1e64f8148a52
Cam Smith <Cam-Smith@live.nmit.ac.nz>
2019-07-26 09:55:47 +1200

    Completed Tests

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -1,72 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Bank
+namespace BankAccountNS
 {
-    namespace BankAccountNS
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
     {
-        /// <summary>
-        /// Bank account demo class.
-        /// </summary>
-        public class BankAccount
+        private readonly string m_customerName;
+        private double m_balance;
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+        private BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
         {
-            private readonly string m_customerName;
-            private double m_balance;
+            m_customerName = customerName;
+            m_balance = balance;
+        }
 
-            private BankAccount() { }
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
 
-            public BankAccount(string customerName, double balance)
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
             {
-                m_customerName = customerName;
-                m_balance = balance;
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
 
-            public string CustomerName
+            if (amount < 0)
             {
-                get { return m_customerName; }
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
-            public double Balance
+            m_balance -= amount; // intentionally incorrect code
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
             {
-                get { return m_balance; }
+                throw new ArgumentOutOfRangeException("amount");
             }
 
-            public void Debit(double amount)
-            {
-                if (amount > m_balance)
-                {
-                    throw new ArgumentOutOfRangeException("amount");
-                }
+            m_balance += amount;
+        }
 
-                if (amount < 0)
-                {
-                    throw new ArgumentOutOfRangeException("amount");
-                }
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
 
-                m_balance += amount; // intentionally incorrect code
-            }
-
-            public void Credit(double amount)
-            {
-                if (amount < 0)
-                {
-                    throw new ArgumentOutOfRangeException("amount");
-                }
-
-                m_balance += amount;
-            }
-
-            public static void Main()
-            {
-                BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
-
-                ba.Credit(5.77);
-                ba.Debit(11.22);
-                Console.WriteLine("Current balance is ${0}", ba.Balance);
-            }
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
         }
     }
 }
